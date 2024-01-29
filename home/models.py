3b816ae51eb33662373f34313a2108ff5efa4f0c
hamed <hamed.alizadegan@gmail.com>
2024-01-29 12:45:39 +0330

    update category model

--- home/models.py
+++ home/models.py
@@ -72,7 +72,7 @@ class MoreLandingModel(models.Model):
 
 class TeamCategoryModel(models.Model):
     header = models.CharField(max_length=2040)
-    priority = models.IntegerField(default=1)
+    priority = models.IntegerField(max_length=100)
     def __str__(self):
         return f'{self.header}'
 class TeamCategoryMemberModel(models.Model):
